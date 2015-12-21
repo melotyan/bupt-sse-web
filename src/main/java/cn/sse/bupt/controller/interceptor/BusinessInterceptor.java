822af7c1da8a1bf4140cd3c02a3a4e5f3044e903
hao.yan <hao.yan@dajie-inc.com>
2015-12-21 19:05:01 +0800

    新增一个业务截器，用来拦截非工作人员

---
+++ src/main/java/cn/sse/bupt/controller/interceptor/BusinessInterceptor.java
@@ -0,0 +1,28 @@
+package cn.sse.bupt.controller.interceptor;
+
+import cn.sse.bupt.common.SessionConstants;
+import cn.sse.bupt.enums.UserTypeEnum;
+import cn.sse.bupt.model.UserModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by hao.yan on 2015/12/15.
+ */
+public class BusinessInterceptor extends BaseInterceptor {
+    private final static Logger LOGGER = LoggerFactory.getLogger(BusinessInterceptor.class);
+
+    @Override
+    public boolean doPreHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        UserModel userModel = (UserModel) request.getSession().getAttribute(SessionConstants.USER);
+        if (userModel == null || userModel.getUserType() != UserTypeEnum.CLERK.getValue()) {
+            LOGGER.debug("user is null or user is not a clerk");
+            return false;
+        }
+        return true;
+    }
+}
