2b7ffca6a9620deca29fdfcfd0d65934d83a49a4
hao.yan <hao.yan@dajie-inc.com>
2015-12-22 10:55:27 +0800

    forward--->redirect

--- src/main/java/cn/sse/bupt/controller/interceptor/LoginInterceptor.java
+++ src/main/java/cn/sse/bupt/controller/interceptor/LoginInterceptor.java
@@ -4,6 +4,7 @@ import cn.sse.bupt.common.SessionConstants;
 import cn.sse.bupt.enums.AccountStatusEnum;
 import cn.sse.bupt.model.UserModel;
 import cn.sse.bupt.service.UserService;
+import cn.sse.bupt.util.CookieUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,21 +20,20 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class LoginInterceptor extends BaseInterceptor {
     private final static Logger LOGGER = LoggerFactory.getLogger(LoginInterceptor.class);
-
+    private final String LOGIN_URL = "/egovernment/userService/preLogin";
 
     @Override
     public boolean doPreHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         UserModel userModel = (UserModel) request.getSession().getAttribute(SessionConstants.USER);
         if (userModel == null) {
             LOGGER.debug("has not login, direct to login page");
-            String lastUrl = String.valueOf(request.getRequestURL());
+            Object lastUrl = request.getRequestURL();
             LOGGER.debug("after login, redirect to {}", lastUrl);
             request.getSession().setAttribute(SessionConstants.LAST_URL, lastUrl);
-            request.getRequestDispatcher("/egovernment/userService/preLogin").forward(request, response);
+            response.sendRedirect(LOGIN_URL);
             return false;
         }
         return true;
     }
-
 
 }
