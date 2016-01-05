faaeee006b6979b136f6fae132121fa62dfe0129
hao.yan <hao.yan@dajie-inc.com>
2016-01-05 10:53:05 +0800

    添加jQuery

---
+++ src/main/webapp/resources/js/user.js
@@ -0,0 +1,21 @@
+/**
+ * Created by hao.yan on 2016/1/4.
+ */
+function changeImg() {
+    var imgSrc = $("#imgObj");
+    var src = imgSrc.attr("src");
+    imgSrc.attr("src", chgUrl(src));
+}
+//时间戳
+//为了使每次生成图片不一致，即不让浏览器读缓存，所以需要加上时间戳
+function chgUrl(url) {
+    var timestamp = (new Date()).valueOf();
+    url = url.substring(0, 17);
+    if ((url.indexOf("&") >= 0)) {
+        url = url + "×tamp=" + timestamp;
+    } else {
+        url = url + "?timestamp=" + timestamp;
+    }
+    return url;
+}
+
