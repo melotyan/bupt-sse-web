b1020eef5fdbf2089ec9274b9affedac36b926f3
melot <melotyan@users.noreply.github.com>
2016-04-06 21:22:20 +0800

    添加内容列表隔行显示不同背景色的js

--- src/main/webapp/resources/js/notice.js
+++ src/main/webapp/resources/js/notice.js
@@ -2,19 +2,23 @@
  * Created by hao.yan on 2016/1/7.
  */
 j = 1;
-$(document).ready(function(){
-
-    $("#btn_add").click(function(){
-        //document.getElementById("upload").innerHTML+='<div id="div_'+j+'"><input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/></div>';
-        //document.getElementById("upload").innerHTML+='<div id="div_'+j+'"><input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/></div>';
-        var div = document.createElement("div");
-        div.id = 'div_' + j;
-        div.innerHTML+='<input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/>';
-        document.getElementById("upload").appendChild(div);
-        j = j + 1;
-    });
+$("#btn_add").click(function(){
+    //document.getElementById("upload").innerHTML+='<div id="div_'+j+'"><input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/></div>';
+    //document.getElementById("upload").innerHTML+='<div id="div_'+j+'"><input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/></div>';
+    var div = document.createElement("div");
+    div.id = 'div_' + j;
+    div.innerHTML+='<input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/>';
+    document.getElementById("upload").appendChild(div);
+    j = j + 1;
 });
+
+
 
 function del(o){
     document.getElementById("upload").removeChild(document.getElementById("div_"+o));
 }
+$(".list-detail:even").css("background", "#FBF5D9");
+$(".list-detail div:odd").css("background", "#F2F6F9");
+
+
+
