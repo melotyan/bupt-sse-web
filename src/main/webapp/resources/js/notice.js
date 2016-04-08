fb3135cca306517819b6661d34eb74cdd9dee998
melot <melotyan@users.noreply.github.com>
2016-04-08 21:16:45 +0800

    可以发布新闻和上传文件了,但是文件上传目前还有乱码

--- src/main/webapp/resources/js/notice.js
+++ src/main/webapp/resources/js/notice.js
@@ -3,22 +3,72 @@
  */
 j = 1;
 $("#btn_add").click(function(){
-    //document.getElementById("upload").innerHTML+='<div id="div_'+j+'"><input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/></div>';
-    //document.getElementById("upload").innerHTML+='<div id="div_'+j+'"><input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/></div>';
-    var div = document.createElement("div");
-    div.id = 'div_' + j;
-    div.innerHTML+='<input  name="files" type="file"  /><input type="button" value="删除"  onclick="del('+j+')"/>';
-    document.getElementById("upload").appendChild(div);
+    var div = $("<div></div>");
+    div.attr("id", 'div_' + j);
+    div.html('<input class="file" name="files" type="file"/><input class="del-btn" type="button" value="删除" onclick="del('+j+')"/>');
+    $("#upload-form").append(div);
     j = j + 1;
 });
 
-
-
 function del(o){
-    document.getElementById("upload").removeChild(document.getElementById("div_"+o));
+    $("#div_" + o).remove();
 }
-$(".list-detail:even").css("background", "#FBF5D9");
-$(".list-detail div:odd").css("background", "#F2F6F9");
+
+//$(".list-detail:even").css("background", "#FBF5D9");
+//$(".list-detail:odd").css("background", "#F2F6F9");
+//$(".list-detail:even").hover(function() {
+//    $('.list-detail').css('background', 'yellow');
+//}, function() {
+//    $(".list-detail:even").css("background", "#FBF5D9");
+//});
+//$(".list-detail:odd").hover(function() {
+//    $('.list-detail:odd').css('background', 'yellow');
+//}, function() {
+//    $(".list-detail:odd").css("background", "#F2F6F9");
+//})
+
+
+$("#btn-notice").click(function() {
+    $(".del-btn").remove();
+    $("#btn_add").remove();
+    var formData = new FormData($("#upload-form")[0]);
+    $.ajax({
+        url: "/fileService/uploadFiles",
+        type: "POST",
+        dataType: "text",
+        contentType: false,
+        processData: false,
+        async: false,
+        cache: false,
+        data: formData,
+        success: function(strVal) {
+            $("#file-urls").val(strVal);
+            console.log(strVal);
+            publish();
+        },
+        error: function(err) {
+            alert("文件上传失败");
+        }
+    });
+
+});
+
+function publish() {
+    $.ajax( {
+        url: '/noticeService/publishNotice',
+        type: 'POST',
+        dataType: 'json',
+        data: $('#notice_publish').serialize(),
+        success: function (json) {
+            alert("发布成功");
+            location.href='/noticeService/listAllNotices/1';
+        },
+        err: function(err) {
+            alert("发布失败");
+        }
+    })
+}
+
 
 
 
